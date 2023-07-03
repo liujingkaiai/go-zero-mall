0ad85f63abd41a6cbc8f1109bc1f408e4611c4b0
whales <166683303@qq.com>
2023-07-03 17:18:26 +0800

    feat:api.api,order.proto,product.proto generate

---
+++ apps/product/rpc/internal/svc/servicecontext.go
@@ -0,0 +1,13 @@
+package svc
+
+import "github.com/liujingkaiai/go-zero-mall/apps/product/rpc/internal/config"
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
