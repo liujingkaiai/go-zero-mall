a89ec71a3d5cfb1d685bbf933f2989cb59247d22
whales <166683303@qq.com>
2023-07-12 15:02:44 +0800

    feat:seckill kafka rmq

--- apps/seckill/rmq/internal/service/service.go
+++ apps/seckill/rmq/internal/service/service.go
@@ -1,7 +1,12 @@
 package service
 
 import (
+	"context"
+	"encoding/json"
+
+	"github.com/liujingkaiai/go-zero-mall/apps/order/rpc/order"
 	"github.com/liujingkaiai/go-zero-mall/apps/order/rpc/orderclient"
+	"github.com/liujingkaiai/go-zero-mall/apps/product/rpc/product"
 	"github.com/liujingkaiai/go-zero-mall/apps/product/rpc/productclient"
 	"github.com/liujingkaiai/go-zero-mall/apps/seckill/rmq/internal/config"
 	"github.com/zeromicro/go-zero/core/logx"
@@ -29,15 +34,39 @@ func NewService(c config.Config) *Service {
 
 func (s *Service) Consume(_ string, value string) (err error) {
 	logx.Info("Consume value:%s \n", value)
-	// var data KafkaData
-	// if err = json.Unmarshal([]byte(value), &data); err != nil {
-	// 	return
-	// }
+	var data KafkaData
+	if err = json.Unmarshal([]byte(value), &data); err != nil {
+		return
+	}
 
-	// p, err := s.ProductRPC.Product(context.Background(), &product.ProductItemRequest{ProductId: data.Pid})
-	// if err != nil {
-	// 	return err
-	// }
+	p, err := s.ProductRPC.Product(context.Background(), &product.ProductItemRequest{ProductId: data.Pid})
+	if err != nil {
+		return err
+	}
+
+	if p.Stock <= 0 {
+		return nil
+	}
+
+	_, err = s.OrderPRC.CreateOrder(context.Background(), &order.CreateOrderRequest{
+		Uid: data.Uid,
+		Pid: data.Pid,
+	})
+
+	if err != nil {
+		logx.Errorf("CreateOrder uid: %d pid: %d error: %v", data.Uid, data.Pid, err)
+		return
+	}
+
+	_, err = s.ProductRPC.UpdateProductStock(context.Background(), &product.UpdateProductStockRequest{
+		ProductId: data.Pid,
+		Num:       1,
+	})
+
+	if err != nil {
+		logx.Errorf("UpdateProductStock uid: %d pid: %d error: %v", data.Uid, data.Pid, err)
+		return err
+	}
 
 	return
 }
--- apps/seckill/rmq/seckill.go
+++ apps/seckill/rmq/seckill.go
@@ -2,8 +2,11 @@ package rmq
 
 import (
 	"flag"
+	"fmt"
 
 	"github.com/liujingkaiai/go-zero-mall/apps/seckill/rmq/internal/config"
+	"github.com/liujingkaiai/go-zero-mall/apps/seckill/rmq/internal/service"
+	"github.com/zeromicro/go-queue/kq"
 	"github.com/zeromicro/go-zero/core/conf"
 )
 
@@ -14,5 +17,11 @@ func main() {
 
 	var c config.Config
 	conf.MustLoad(*configFile, &c)
+	srv := service.NewService(c)
 
+	queue := kq.MustNewQueue(c.Kafka, kq.WithHandle(srv.Consume))
+	defer queue.Stop()
+
+	fmt.Println("seckill started!!!")
+	queue.Start()
 }
