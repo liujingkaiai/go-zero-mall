e567c55fca4684a19900dd84b04b059b32e76e6f
whales <166683303@qq.com>
2023-07-13 16:55:50 +0800

    feat:order rpc create order

--- apps/order/rpc/internal/logic/createorderlogic.go
+++ apps/order/rpc/internal/logic/createorderlogic.go
@@ -2,6 +2,10 @@ package logic
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"sync/atomic"
+	"time"
 
 	"github.com/liujingkaiai/go-zero-mall/apps/order/rpc/internal/svc"
 	"github.com/liujingkaiai/go-zero-mall/apps/order/rpc/order"
@@ -25,6 +29,33 @@ func NewCreateOrderLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Creat
 
 func (l *CreateOrderLogic) CreateOrder(in *order.CreateOrderRequest) (*order.CreateOrderResponse, error) {
 	// todo: add your logic here and delete this line
-
+	oid := genOrderID(time.Now())
+	err := l.svcCtx.OrderModel.CreateOrder(l.ctx, oid, in.Uid, in.Pid)
+	if err != nil {
+		return nil, err
+	}
 	return &order.CreateOrderResponse{}, nil
+}
+
+var num int64
+
+func genOrderID(t time.Time) string {
+	s := t.Format("20060102150405")
+	m := t.UnixNano()/1e6 - t.UnixNano()/1e9*1e3
+	ms := sup(m, 3)
+	p := os.Getpid() % 1000
+	ps := sup(int64(p), 3)
+	i := atomic.AddInt64(&num, 1)
+	r := i % 10000
+	rs := sup(r, 4)
+	n := fmt.Sprintf("%s%s%s%s", s, ms, ps, rs)
+	return n
+}
+
+func sup(i int64, n int) string {
+	m := fmt.Sprintf("%d", i)
+	for len(m) < n {
+		m = fmt.Sprintf("0%s", m)
+	}
+	return m
 }
--- apps/order/rpc/internal/svc/servicecontext.go
+++ apps/order/rpc/internal/svc/servicecontext.go
@@ -1,13 +1,20 @@
 package svc
 
-import "github.com/liujingkaiai/go-zero-mall/apps/order/rpc/internal/config"
+import (
+	"github.com/liujingkaiai/go-zero-mall/apps/order/rpc/internal/config"
+	"github.com/liujingkaiai/go-zero-mall/apps/order/rpc/internal/model"
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+)
 
 type ServiceContext struct {
-	Config config.Config
+	Config     config.Config
+	OrderModel model.OrdersModel
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+	conn := sqlx.NewMysql(c.DataSource)
 	return &ServiceContext{
-		Config: c,
+		Config:     c,
+		OrderModel: model.NewOrdersModel(conn, c.CacheRedis),
 	}
 }
